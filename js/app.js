0e0391abc78cbb8c37e580633bb0dc7f63d40a82
Anna Magdalena Bloeckner <m.bloeckner@web.de>
2020-03-01 18:56:16 +0100

    jump to section works

--- js/app.js
+++ js/app.js
@@ -28,21 +28,87 @@ function getNumberOfSections() {
   return sections.length;
 }
 
-function createNavItem(sectionNr) {
+/* function createNavItem(navItemText, index) {
   const newLi = document.createElement("li");
-  const sectionId = "section" + sectionNr;
-  newLi.innerHTML =
-    "<a href='#" + sectionId + "'>Section " + sectionNr + "</a>";
+  const sectionId = "section" + index;
+  newLi.innerHTML = "<a href='#" + sectionId + "'>Section " + index + "</a>";
+  //newLi.innerHTML = "<a href='#" + sectionId + "'>" + navItemText + "</a>";
+  newLi.className = "menu__link";
   return newLi;
+} */
+
+function addLinkToNav(section) {
+  const navbarlist = document.getElementById("navbar__list");
+
+  const newLi = document.createElement("li");
+  const navItemText = section.getAttribute("data-nav");
+
+  newLi.textContent = navItemText;
+  newLi.className = "menu__link";
+
+  newLi.addEventListener("click", function(event) {
+    console.log("Navitem " + navItemText + " clicked!");
+    event.preventDefault();
+    //section.scrollIntoView({ left: 0, block: "start", behavior: "smooth" });
+    //const yOffset = 10;
+    //document.getElementById("page__header")
+    //.getBoundingClientRect.height;
+    const yOffset_custom = 0;
+    const yOffset_nav = -navbarlist.getBoundingClientRect().height; //-200;
+    const yOffset = yOffset_nav + yOffset_custom;
+    const yPos =
+      section.getBoundingClientRect().top + window.pageYOffset + yOffset;
+    //window.scrollTo(0, yPos);
+    window.scrollTo({ top: yPos, behavior: "smooth" });
+    //window.scrollTo({ y: yPos, behavior: "smooth" });
+    /*     const yOffset = 0; //-10;
+    //const element = document.getElementById(id);
+    const yPos =
+      section.getBoundingClientRect().top + window.pageYOffset + yOffset;
+    section.scrollTo({ y: yPos, behavior: "smooth" }); */
+  });
+  /*   const sectionId = "section" + sectionNr;
+  newLi.innerHTML = "<a href='#" + sectionId + "'>" + navItemText + "</a>"; */
+
+  navbarlist.appendChild(newLi);
+  //return newLi;
 }
 
 function buildStarterNav() {
-  const navbarlist = document.getElementById("navbar__list");
-  for (let i = 1; i <= 3; i++) {
+  //const navbarlist = document.getElementById("navbar__list");
+  const sections = document.getElementsByTagName("section");
+
+  for (const section of sections) {
+    const newLi = addLinkToNav(section);
+    //navbarlist.appendChild(newLi);
+  }
+
+  /*   sections.forEach(element => {
+    const newLi = createNavItem(element.getAttribute("data-nav"), index);
+    navbarlist.appendChild(newLi);
+    i++;
+  }); */
+
+  /*   for (let i = 1; i <= 3; i++) {
+    //TODO: get section, retrieve attribute data-nav
+    //change createNavItem: pass whole section
+    //add eventlistener in createNavItem
     const newLi = createNavItem(i);
     navbarlist.appendChild(newLi);
-  }
+  } */
 }
+/*
+  sections.forEach(element => {
+    const newLi = createNavItem(element.getAttribute("data-nav"));
+  });
+  function createNavItem(navItemText) {
+    const newLi = document.createElement("li");
+    const sectionId = "section" + sectionNr;
+    newLi.innerHTML = "<a href='#" + sectionId + "'>" + navItemText + "</a>";
+    newLi.className = "menu__link";
+    return newLi;
+  }
+*/
 
 function addNewSection() {
   //get the number of the new section
@@ -88,9 +154,8 @@ function addNewSection() {
   mainItem.appendChild(newSection);
 
   //add a new nav item for the new section
-  const navbarlist = document.getElementById("navbar__list");
-  const newLi = createNavItem(sectionNr);
-  navbarlist.appendChild(newLi);
+  const newLi = addLinkToNav(newSection);
+  //navbarlist.appendChild(newLi);
 }
 
 /**
@@ -104,13 +169,58 @@ console.log("Initial number of sections = ", getNumberOfSections());
 // build nav for the existing sections
 buildStarterNav();
 console.log("Initial Navigation built");
-//add 10 new sections to the page
-for (let i = 1; i <= 10; i++) {
+//add 3 new sections to the page
+for (let i = 1; i <= 3; i++) {
   addNewSection();
   console.log(i + ". new section added");
 }
 
 // Add class 'active' to section when near top of viewport
+//////////////////////////////
+function isElementInViewport(element) {
+  var rect = element.getBoundingClientRect();
+  return (
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <=
+      (window.innerHeight || document.documentElement.clientHeight) &&
+    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
+}
+
+//var elements = document.querySelectorAll("section");
+const sections = document.getElementsByTagName("section");
+
+function callbackFunc() {
+  console.log("LOAD or SCROLL");
+  for (let i = 0; i < sections.length; i++) {
+    const section = sections[i];
+    console.log(
+      section.getAttribute("data-nav") +
+        " is in viewport: " +
+        isElementInViewport(section)
+    );
+    /*     if (isElementInViewport(section)) {
+      console.log(section.getAttribute("data-nav") + " is in viewport");
+    } else {
+      console.log(section.getAttribute("data-nav"));
+    } */
+  }
+  /*
+  for (var i = 0; i < sections.length; i++) {
+    if (isElementInViewport(sections[i])) {
+      sections[i].classList.add("your-active-class");
+    }
+         else {
+      //Else-Bedinung entfernen, um .visible nicht wieder zu löschen, wenn das Element den Viewport verlässt.
+      sections[i].classList.remove("your-active-class");
+    } 
+  } */
+}
+
+window.addEventListener("load", callbackFunc);
+window.addEventListener("scroll", callbackFunc);
+//////////////////////////////
 //toggle class
 
 // Scroll to anchor ID using scrollTO event
@@ -124,5 +234,9 @@ for (let i = 1; i <= 10; i++) {
 // Build menu
 
 // Scroll to section on link click
+function crollIntoView(event, section) {
+  event.preventDefault();
+  section.scrollIntoView({ left: 0, block: "start", behavior: "smooth" });
+}
 
 // Set sections as active
